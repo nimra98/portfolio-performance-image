69b66449e209de4e69a66d8fbc2fe84c08a091a0
Armin Jacob <armin.jacob@yahoo.de>
2024-08-08 18:46:18 +0200

    local testing new version

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker push quallenbezwinger/portfolio-performance:0.9
+docker push quallenbezwinger/portfolio-performance:0.10
 docker push quallenbezwinger/portfolio-performance:latest
--- localImage.sh
+++ localImage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -t quallenbezwinger/portfolio-performance:0.9
+docker build . -t quallenbezwinger/portfolio-performance:0.10
 docker build . -t quallenbezwinger/portfolio-performance:latest
