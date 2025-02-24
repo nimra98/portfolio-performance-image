81cd7d620a606a725a7b6ca2c847e5f32b4e9a73
Armin Jacob <armin.jacob@yahoo.de>
2025-02-24 18:48:47 +0100

    add more release options

--- make.sh
+++ make.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Check if VERSION is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 VERSION"
+# Check if VERSION and RELEASE_TYPE are provided
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 VERSION RELEASE_TYPE"
+    echo "Example: $0 1.0.0 release-amd64"
+    echo "Example: $0 1.0.0 release"
     exit 1
 fi
 
 VERSION=$1
+RELEASE_TYPE=$2
 
-sudo make release-amd64 VERSION=$VERSION LATEST=true PACKAGING=pponly
-sudo make release-amd64 VERSION=$VERSION LATEST=true PACKAGING=nextcloud
-sudo make release-amd64 VERSION=$VERSION LATEST=true PACKAGING=firefox
-sudo make release-amd64 VERSION=$VERSION LATEST=true PACKAGING=firefox-nextcloud
-
-sudo make release-arm64 VERSION=$VERSION LATEST=true PACKAGING=pponly
-sudo make release-arm64 VERSION=$VERSION LATEST=true PACKAGING=nextcloud
-sudo make release-arm64 VERSION=$VERSION LATEST=true PACKAGING=firefox
-sudo make release-arm64 VERSION=$VERSION LATEST=true PACKAGING=firefox-nextcloud
+sudo make $RELEASE_TYPE VERSION=$VERSION LATEST=true PACKAGING=pponly
+sudo make $RELEASE_TYPE VERSION=$VERSION LATEST=true PACKAGING=nextcloud
+sudo make $RELEASE_TYPE VERSION=$VERSION LATEST=true PACKAGING=firefox
+sudo make $RELEASE_TYPE VERSION=$VERSION LATEST=true PACKAGING=firefox-nextcloud
